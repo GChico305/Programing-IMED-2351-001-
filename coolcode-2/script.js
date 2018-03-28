1d29dfbef722ce73ab2da47d4033a1b7d5917482
Geo Bustamante-Gooden <35774812+GChico305@users.noreply.github.com>
2018-03-27 21:29:40 -0500

    Cool code 2
    Is ready to upload! =)

--- coolcode-2/script.js
+++ coolcode-2/script.js
@@ -29,11 +29,17 @@ $('#blurmethree').mouseleave(function(){
 		});
 
 
-$(document).ready(function(){
-    $("button").click(function(){
-        var div = $("h4");  
-        div.animate({right: '1000px'}, "slow");
-        div.animate({fontSize: '3em'}, "slow");
-    });
-});
 
+
+	   
+var messages = new Array(6); 
+messages[0] = "";
+messages[1] = "Geo has visited 10 countries";
+messages[2] = "Geo hates shopping";	   
+messages[3] = "Geo loves to eat food from different nationalities";
+messages[4] = "Geo loves dogs and he has two of his own";
+messages[5] = "Geo's three top drinks are coffee, water and liquor";
+function messageReveal() {
+var messageindex = document.messageForm.messagePick.selectedIndex
+document.messageForm.messageField.value = messages[messageindex];
+}
